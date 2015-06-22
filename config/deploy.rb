5b87ba099d79557164dca448ecd123efc2a963fd
Chris Lerum <chris.lerum@gmail.com>
2015-06-22 16:16:55 -0400

    x

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :chruby_ruby, '2.2.0'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 #set :default_env, { path: "/opt/ruby/bin:$PATH" }
