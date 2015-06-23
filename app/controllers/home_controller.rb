4fb47989c4a5eaa0a4c8f803a9040526fe05de4d
Chris Lerum <chris.lerum@gmail.com>
2015-06-23 12:24:26 -0400

    add unpublished view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.where(published: true)
   end
 
   def show_post
     @post = Post.friendly.find(params[:id])
+  end
+
+  def unpublished
+    @posts = Post.all.where(published: false)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   get 'show_post/:id' => 'home#show_post', as: :show_post
+  get 'unpublished' => 'home#unpublished', as: :unpublished
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
